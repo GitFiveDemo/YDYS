400537a41474117f81cbd2bd1fa4ddaf838fb6ca
baby0 <1277704990@qq.com>
2018-05-18 21:23:47 +0800

    更新专题内容

---
+++ app/src/main/java/ydys/jinou/com/view/activity/SpecialDetailActivity.java
@@ -0,0 +1,79 @@
+package ydys.jinou.com.view.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+
+import com.google.gson.Gson;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import ydys.jinou.com.R;
+import ydys.jinou.com.model.bean.SpeciaDetailBean;
+import ydys.jinou.com.presenter.HomePresenter;
+import ydys.jinou.com.view.adapter.SpecialDetailAdapter;
+import ydys.jinou.com.view.base.BaseActivity;
+import ydys.jinou.com.view.base.BaseMVPActivity;
+import ydys.jinou.com.view.callback.SimpleCallBack;
+
+public class SpecialDetailActivity extends BaseMVPActivity<HomePresenter> implements SimpleCallBack<String> {
+
+    @BindView(R.id.detail_recy_view)
+    RecyclerView detailRecyView;
+    private HomePresenter homePresenter;
+
+    @Override
+    protected void initView() {
+        detailRecyView.setLayoutManager(new GridLayoutManager(this,3));
+    }
+
+    @Override
+    protected void initDate() {
+    }
+
+
+    @Override
+    protected HomePresenter getPresenter() {
+        homePresenter = new HomePresenter();
+        return homePresenter;
+    }
+
+
+    @Override
+    protected View getMVPView() {
+        return View.inflate(this,R.layout.activity_special_detail,null);
+    }
+
+    @Override
+    protected void functionExtension() {
+        //获取传过来的路径
+        Intent intent = getIntent();
+        String loadURL = intent.getStringExtra("loadURL");
+        if (loadURL != null) {
+            homePresenter.getData(loadURL,null);
+        }
+
+    }
+
+    @Override
+    public void succeed(String s) {
+        SpeciaDetailBean speciaDetailBean = new Gson().fromJson(s, SpeciaDetailBean.class);
+        List<SpeciaDetailBean.RetBean.ListBean> list = speciaDetailBean.getRet().getList();
+        Log.d("SpecialDetailActivity", "获取到专题详情集合-------------"+list);
+        SpecialDetailAdapter specialDetailAdapter = new SpecialDetailAdapter(this, list);
+        detailRecyView.setAdapter(specialDetailAdapter);
+    }
+
+    @Override
+    public void failure(String error) {
+        Log.d("SpecialDetailActivity", "出现异常===================="+error);
+    }
+}
--- app/src/main/java/ydys/jinou/com/view/adapter/SpecialAdapter.java
+++ app/src/main/java/ydys/jinou/com/view/adapter/SpecialAdapter.java
@@ -1,10 +1,13 @@
 package ydys.jinou.com.view.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.facebook.drawee.view.SimpleDraweeView;
 
@@ -12,6 +15,7 @@ import java.util.List;
 
 import ydys.jinou.com.R;
 import ydys.jinou.com.model.bean.SpeciaBean;
+import ydys.jinou.com.view.activity.SpecialDetailActivity;
 
 public class SpecialAdapter extends RecyclerView.Adapter<SpecialAdapter.MySpecialViewHolder> {
    private Context context;
@@ -31,8 +35,18 @@ public class SpecialAdapter extends RecyclerView.Adapter<SpecialAdapter.MySpecia
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MySpecialViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MySpecialViewHolder holder, final int position) {
         holder.simpleDraweeView.setImageURI(list.get(position).getPic());
+        holder.simpleDraweeView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, SpecialDetailActivity.class);
+                intent.putExtra("loadURL",list.get(position).getLoadURL());
+                context.startActivity(intent);
+            }
+        });
+        holder.relative_layout.getBackground().setAlpha(100);
+        holder.text.setText(list.get(position).getTitle());
     }
 
     @Override
@@ -43,10 +57,14 @@ public class SpecialAdapter extends RecyclerView.Adapter<SpecialAdapter.MySpecia
     class MySpecialViewHolder extends RecyclerView.ViewHolder {
 
         private final SimpleDraweeView simpleDraweeView;
+        private RelativeLayout relative_layout;
+        private final TextView text;
 
         public MySpecialViewHolder(View itemView) {
             super(itemView);
             simpleDraweeView = itemView.findViewById(R.id.simp_view);
+            relative_layout = itemView.findViewById(R.id.relative_layout);
+            text = itemView.findViewById(R.id.text);
         }
     }
 }
