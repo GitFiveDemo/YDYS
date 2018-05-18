8e9db8f50be70e73bbe853580340beaa9a2aeb24
wangzhanqin <1309176968@qq.com>
2018-05-18 11:36:06 +0800

    Home updatesssssss

---
+++ app/src/main/java/ydys/jinou/com/view/adapter/MyAdapters.java
@@ -0,0 +1,53 @@
+package ydys.jinou.com.view.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+/**
+ * Created by 王有道 on 2018/4/9.
+ */
+
+public class MyAdapters extends FragmentPagerAdapter {
+
+    List<Fragment> list;
+    List<String> data ;
+    public MyAdapters(FragmentManager fm, List<Fragment> list, List<String> data) {
+        super(fm);
+        this.data=data;
+        this.list=list;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return list.get(position);
+    }
+
+    public List<Fragment> getList() {
+        return list;
+    }
+
+    public void setList(List<Fragment> list) {
+        this.list = list;
+    }
+
+    public List<String> getData() {
+        return data;
+    }
+
+    public void setData(List<String> data) {
+        this.data = data;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return  data.get(position);
+    }
+}
