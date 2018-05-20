2b0f7b38994fa446e87f7641aa85648ce0e5f228
baby0 <1277704990@qq.com>
2018-05-20 18:51:56 +0800

    更新专题内容

--- app/src/main/java/ydys/jinou/com/view/activity/SettingActivity.java
+++ app/src/main/java/ydys/jinou/com/view/activity/SettingActivity.java
@@ -6,6 +6,8 @@ import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
--- app/src/main/java/ydys/jinou/com/view/adapter/SpecialDetailAdapter.java
+++ app/src/main/java/ydys/jinou/com/view/adapter/SpecialDetailAdapter.java
@@ -5,7 +5,9 @@ import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.facebook.drawee.view.SimpleDraweeView;
 
 import java.util.List;
@@ -33,21 +35,28 @@ public class SpecialDetailAdapter extends RecyclerView.Adapter<SpecialDetailAdap
 
     @Override
     public void onBindViewHolder(@NonNull SpecialDetailViewHolder holder, int position) {
-        holder.simpleDraweeView.setImageURI(list.get(position).getChildList().get(0).getPic());
+        holder.simpleDraweeView.setImageURI(list.get(0).getChildList().get(position).getPic());
+
+//        Glide.with(context).load(list.get(0).getChildList().get(position).getPic()).into(holder.simpleDraweeView);
+
     }
+
 
     @Override
     public int getItemCount() {
-        return list.size();
+        return list.get(0).getChildList().size();
     }
 
     class SpecialDetailViewHolder extends RecyclerView.ViewHolder {
+//        private final ImageView simpleDraweeView;
 
         private final SimpleDraweeView simpleDraweeView;
 
         public SpecialDetailViewHolder(View itemView) {
             super(itemView);
             simpleDraweeView = itemView.findViewById(R.id.simp_detail_view);
+//            simpleDraweeView = itemView.findViewById(R.id.simp_detail_view);
         }
+
     }
 }
