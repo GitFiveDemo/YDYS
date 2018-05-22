f8b8acb45b8346f623bf5f9fab6cb126ab730995
chenguang071116 <294633912@qq.com>
2018-05-22 18:52:53 +0800

    add change theme

--- app/src/main/java/ydys/jinou/com/view/activity/MainActivity.java
+++ app/src/main/java/ydys/jinou/com/view/activity/MainActivity.java
@@ -1,54 +1,42 @@
 package ydys.jinou.com.view.activity;
 
 
-import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.GridView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.ashokvarma.bottomnavigation.BottomNavigationBar;
 import com.ashokvarma.bottomnavigation.BottomNavigationItem;
-import com.iflytek.cloud.ErrorCode;
-import com.iflytek.cloud.InitListener;
-import com.iflytek.cloud.RecognizerListener;
-import com.iflytek.cloud.RecognizerResult;
-import com.iflytek.cloud.SpeechConstant;
-import com.iflytek.cloud.SpeechError;
-import com.iflytek.cloud.SpeechRecognizer;
-import com.iflytek.cloud.SpeechUtility;
-import com.iflytek.cloud.ui.RecognizerDialog;
-import com.iflytek.cloud.ui.RecognizerDialogListener;
+import com.jaeger.library.StatusBarUtil;
 
 import org.greenrobot.eventbus.EventBus;
-import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import ydys.jinou.com.R;
 import ydys.jinou.com.broadcast.NetBroadcastReceiver;
-import ydys.jinou.com.util.JsonParser;
+import ydys.jinou.com.util.CommonUtil;
+import ydys.jinou.com.view.adapter.ThemeAdapter;
 import ydys.jinou.com.view.custom.CircleImageView;
 import ydys.jinou.com.view.custom.slide.SlideLayout;
 import ydys.jinou.com.view.fragment.FancyFragment;
@@ -76,46 +64,13 @@ public class MainActivity extends AppCompatActivity {
     private FancyFragment fancyFragment;
     private MineFragment mineFragment;
     private NetBroadcastReceiver netBroadcastReceiver;
-
-    //显示听写结果
-    private TextView textView;
-    //语音听写对象
-    private SpeechRecognizer speechRecognizer;
-    //语音听写UI
-    private RecognizerDialog recognizerDialog;
-    //是否显示听写UI
-    private boolean isShowDialog = true;
-    //缓存
-    private SharedPreferences sharedPreferences;
-    //用hashmap存储听写结果
-    private HashMap<String, String> hashMap = new LinkedHashMap<>();
-    //引擎类型（云端或本地）
-    private String mEngineType = null;
-    //函数返回值
-    private int ret = 0;
-    Toast toast;
-
-    //有UI监听器
-    private RecognizerDialogListener dialogListener = new RecognizerDialogListener() {
-        @Override
-        public void onResult(RecognizerResult recognizerResult, boolean b) {
-            if (recognizerResult != null) {
-                Log.e("tag", "听写结果：" + recognizerResult.getResultString());
-                printResult(recognizerResult);
-            }
-        }
-
-        @Override
-        public void onError(SpeechError speechError) {
-            Log.e("tag", speechError.getPlainDescription(true));
-
-        }
-    };
+    private View parentView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        parentView = View.inflate(this, R.layout.activity_main, null);
+        setContentView(parentView);
         ButterKnife.bind(this);
         init();
         initBottomMenu();
@@ -169,8 +124,7 @@ public class MainActivity extends AppCompatActivity {
 
         // 首页默认隐藏 titlebar
         mainTitle.setVisibility(View.GONE);
-
-        initDateXunFei();
+        StatusBarUtil.setTranslucent(this, 1);
     }
 
     private void initBottomMenu() {
@@ -295,13 +249,24 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.menu_user_about1:
             case R.id.menu_user_about2: // 弹框
-
+                showAboutView();
                 break;
             case R.id.menu_user_theme1: // 弹框 选择主题
             case R.id.menu_user_theme2:
+                showSelectThemes();
                 break;
         }
     }
+
+
+    // 关于
+    private void showAboutView() {
+        new AlertDialog.Builder(this)
+                .setTitle("关于我们")
+                .setMessage("暂无具体内容")
+                .show();
+    }
+
 
     @Override
     protected void onStart() {
@@ -330,6 +295,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 
     @Override
@@ -344,12 +315,11 @@ public class MainActivity extends AppCompatActivity {
     public void showDiglog() {
         final AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
         View alertView = View.inflate(this, R.layout.alertdialog3_view_layout, null);
-                
+
         alertBuilder.setView(alertView);
         alertBuilder.setCancelable(true);
         EditText edit_text_youxiang = alertView.findViewById(R.id.edit_text_youxiang);
         EditText edit_text_fankui = alertView.findViewById(R.id.edit_text_fankui);
-        textView = edit_text_fankui;
         Button btn_ly = alertView.findViewById(R.id.btn_ly);
         Button btn_quxiao = alertView.findViewById(R.id.btn_quxiao);
         Button btn_fasong = alertView.findViewById(R.id.btn_fasong);
@@ -361,12 +331,12 @@ public class MainActivity extends AppCompatActivity {
         btn_ly.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                xunFeiStart();
+                //  xunFeiStart();
                 Toast.makeText(MainActivity.this, "反馈", Toast.LENGTH_SHORT).show();
             }
         });
         phoneInfoTv.setText(phoneInfo);
-        
+
         final AlertDialog alertDialog = alertBuilder.create();
         btn_quxiao.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -388,161 +358,60 @@ public class MainActivity extends AppCompatActivity {
         alertDialog.show();
     }
 
-    private void initDateXunFei() {
-        //初始化sdk 将自己申请的appid放到下面
-        //此句代码应该放在application中的，这里为了方便就直接放代码中了
-      //  SpeechUtility.createUtility(this, "appid=5ae0845d");
-        speechRecognizer = SpeechRecognizer.createRecognizer(this, initListener);
-        Log.e("speechRecognizer","---->"+speechRecognizer);
-        recognizerDialog = new RecognizerDialog(this, initListener);
-        sharedPreferences = getSharedPreferences(this.getPackageName(), Context.MODE_PRIVATE);
-        toast = Toast.makeText(this, "", Toast.LENGTH_SHORT);
-        //这里我直接将引擎类型设置为云端，因为本地需要下载讯飞语记，这里为了方便直接使用云端
-        //有需要的朋友可以加个单选框 让用户选择云端或本地
-        mEngineType = SpeechConstant.TYPE_CLOUD;
+    public ArrayList<Integer> getColorData() {
+        ArrayList<Integer> integers = new ArrayList<>();
+        integers.add(R.color.colorBluePrimaryDark);
+        integers.add(R.color.colorAccent);
+        integers.add(R.color.colorTealPrimary);
+        integers.add(R.color.colorDeepOrangePrimary);
+        integers.add(R.color.colorRedPrimaryCenter);
+        integers.add(R.color.colorRedPrimary);
+        integers.add(R.color.colorPrimaryDark);
+        integers.add(R.color.colorPrimary);
+        integers.add(R.color.colorLimePrimaryCenter);
+        integers.add(R.color.colorOrangePrimary);
+        integers.add(R.color.colorSecondText);
+        integers.add(R.color.colorLimePrimaryDark);
+        integers.add(R.color.colorDeepPurplePrimaryCenter);
+        integers.add(R.color.colorHint);
+        integers.add(R.color.colorDeepOrangePrimaryCenter);
+        integers.add(R.color.colorSecondText);
+
+        return integers;
     }
 
-    //开始听写
-    public void xunFeiStart() {
-        textView.setText("");
-        hashMap.clear();
-        setParams();
+    private int clickPosition = 0;
 
-        if (isShowDialog) {
-            recognizerDialog.setListener(dialogListener);
-            recognizerDialog.show();
-        } else {
-            ret = speechRecognizer.startListening(recognizerListener);
-            if (ret != ErrorCode.SUCCESS) {
-                Log.e("tag", "听写失败,错误码" + ret);
+    public void showSelectThemes() {
+        clickPosition = 0;
+        final ArrayList<Integer> colorData = getColorData();
+        View view = View.inflate(this, R.layout.theme_view, null);
+        GridView gridView = view.findViewById(R.id.theme_gridView);
+        final ThemeAdapter themeAdapter = new ThemeAdapter(getColorData(), 0, this);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                themeAdapter.setPosition(position);
+                clickPosition = position;
+                themeAdapter.notifyDataSetChanged();
             }
-        }
-    }
+        });
+        gridView.setAdapter(themeAdapter);
 
-    //结束听写
-    public void stop() {
-        Toast.makeText(this, "停止听写", Toast.LENGTH_SHORT).show();
-        if (isShowDialog) {
-            recognizerDialog.dismiss();
-        } else {
-            speechRecognizer.stopListening();
-        }
-    }
-
-    //初始化监听器
-    private InitListener initListener = new InitListener() {
-        @Override
-        public void onInit(int i) {
-            if (i != ErrorCode.SUCCESS) {
-                Log.e("tag", "初始化失败，错误码" + i);
-            }
-        }
-    };
-
-    //无UI监听器
-    private RecognizerListener recognizerListener = new RecognizerListener() {
-        @Override
-        public void onVolumeChanged(final int i, byte[] bytes) {
-            Log.e("tag", "返回数据大小" + bytes.length);
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    toast.setText("当前音量" + i);
-                }
-            });
-        }
-
-        @Override
-        public void onBeginOfSpeech() {
-            Log.e("tag", "开始说话");
-        }
-
-        @Override
-        public void onEndOfSpeech() {
-            Log.e("tag", "结束说话");
-        }
-
-        @Override
-        public void onResult(RecognizerResult recognizerResult, boolean b) {
-            if (recognizerResult != null) {
-                Log.e("tag", "听写结果：" + recognizerResult.getResultString());
-                printResult(recognizerResult);
-            }
-
-        }
-
-        @Override
-        public void onError(SpeechError speechError) {
-            Log.e("tag", "错误信息" + speechError.getPlainDescription(true));
-            if (isShowDialog) {
-                recognizerDialog.dismiss();
-            } else {
-                speechRecognizer.stopListening();
-            }
-        }
-
-        @Override
-        public void onEvent(int i, int i1, int i2, Bundle bundle) {
-            // 以下代码用于获取与云端的会话id，当业务出错时将会话id提供给技术支持人员，可用于查询会话日志，定位出错原因
-            // 若使用本地能力，会话id为null
-            //  if (SpeechEvent.EVENT_SESSION_ID == eventType) {
-            //      String sid = obj.getString(SpeechEvent.KEY_EVENT_SESSION_ID);
-            //      Log.d(TAG, "session id =" + sid);
-            //  }
-        }
-    };
-
-
-
-    //输出结果，将返回的json字段解析并在textVie中显示
-    private void printResult(RecognizerResult results) {
-        String text = JsonParser.parseIatResult(results.getResultString());
-        String sn = null;
-        // 读取json结果中的sn字段
-        try {
-            JSONObject resultJson = new JSONObject(results.getResultString());
-            sn = resultJson.optString("sn");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        hashMap.put(sn, text);
-
-        StringBuffer resultBuffer = new StringBuffer();
-        for (String key : hashMap.keySet()) {
-            resultBuffer.append(hashMap.get(key));
-        }
-
-        textView.setText(resultBuffer.toString());
-    }
-
-    private void setParams() {
-        //清空参数
-//        speechRecognizer.setParameter(SpeechConstant.PARAMS, null);
-        //设置引擎
-        speechRecognizer.setParameter(SpeechConstant.ENGINE_TYPE, mEngineType);
-        //设置返回数据类型
-        speechRecognizer.setParameter(SpeechConstant.RESULT_TYPE, "json");
-        //设置中文 普通话
-        speechRecognizer.setParameter(SpeechConstant.LANGUAGE, "zh_cn");
-        speechRecognizer.setParameter(SpeechConstant.ACCENT, "mandarin");
-
-        // 设置语音前端点:静音超时时间，即用户多长时间不说话则当做超时处理
-        speechRecognizer.setParameter(SpeechConstant.VAD_BOS,
-                sharedPreferences.getString("iat_vadbos_preference", "4000"));
-
-        // 设置语音后端点:后端点静音检测时间，即用户停止说话多长时间内即认为不再输入， 自动停止录音
-        speechRecognizer.setParameter(SpeechConstant.VAD_EOS,
-                sharedPreferences.getString("iat_vadeos_preference", "1000"));
-
-        // 设置标点符号,设置为"0"返回结果无标点,设置为"1"返回结果有标点
-        speechRecognizer.setParameter(SpeechConstant.ASR_PTT,
-                sharedPreferences.getString("iat_punc_preference", "0"));
-
-        // 设置音频保存路径，保存音频格式支持pcm、wav，设置路径为sd卡请注意WRITE_EXTERNAL_STORAGE权限
-        // 注：AUDIO_FORMAT参数语记需要更新版本才能生效
-        speechRecognizer.setParameter(SpeechConstant.AUDIO_FORMAT, "wav");
-        speechRecognizer.setParameter(SpeechConstant.ASR_AUDIO_PATH,
-                Environment.getExternalStorageDirectory() + "/msc/iat.wav");
+        new AlertDialog.Builder(this)
+                .setView(view)
+                .setPositiveButton("取消", null)
+                .setNegativeButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        int color = getResources().getColor(colorData.get(clickPosition));
+                        StatusBarUtil.setColor(MainActivity.this, color);
+                        if (parentView != null)
+                            parentView.setBackgroundColor(color);
+                        CommonUtil.saveColorValue(color);
+                    }
+                })
+                .create()
+                .show();
     }
 }
